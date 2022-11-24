8cb4b3c3de543f61102ac00a2de966e9a02ad528
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-11-24 16:35:09 +0700

    add class CircularList & method add

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -12,6 +12,54 @@ namespace Exercise3
         public string studentName;
         public Node next;
     }
+    class CircularList
+    {
+        Node LAST;
+        public CircularList()
+        {
+            LAST = null;
+        }
+        public void addNode()/*Add method*/
+        {
+            int nim;
+            string nm;
+            Console.Write("\nMasukkan nomor Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nMasukkan Nama Mahasiswa: ");
+            nm = Console.ReadLine();
+            Node NodeBaru = new Node();
+            NodeBaru.studentNumber = nim;
+            NodeBaru.studentName = nm;
+            if (LAST == null || nim <= LAST.studentNumber)
+            {
+                if ((LAST != null) && (nim == LAST.studentNumber))
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                NodeBaru.next = LAST;
+                LAST = NodeBaru;
+                return;
+            }
+            /*Menemukan Node baru didalam list*/
+            Node previous, current;
+            previous = LAST;
+            current = LAST;
+            while ((current != null) && (nim >= current.studentNumber))
+            {
+                if (nim == current.studentNumber)
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                previous = current;
+                current = current.next;
+            }
+            /*Node baru ditempatkan diantara previous dan current*/
+            NodeBaru.next = current;
+            previous.next = NodeBaru;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
